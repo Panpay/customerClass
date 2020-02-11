63c28188e7873eb9d7ed06813fa12fc8c22b364e
Panpay <yuxchen@clarkson.edu>
2020-02-11 11:53:44 -0500

    Latest version

--- customer.py
+++ customer.py
@@ -1,8 +1,13 @@
 class customerList:
+    #this is the assignment
     def __init__(self):
         self.data = []
         self.tempdata = {}
+        self.tn = 'customers'
         self.fnl = ['fname','lname','email','password','subscribed']
+        self.conn = None
+    def connect(self):
+        seld.conn = pymysql.connect(host='mysql.clarksonmsda.org', port=3306, user='yuxchen', passwd='Phpmyadmin1', db='yuxchen_is437test', autocommit=True)
     def add(self):
         self.data.append(self.tempdata)
     def set(self,fn,val):
@@ -14,5 +19,19 @@ class customerList:
             if len(self.data) >= (n + 1) and fn in self.fnl:
                 self.data[n][fn] = val
             else:
-                print('could not set value at row ' . str(n) . 'col' . str(fn))
-                
+                print('could not set value at row ' + str(n) + 'col' + str(fn))
+    def insert(self,n=0):
+
+        cols = '","'.join(self.fnl)
+        cols = '"'+cols+'"'
+        vals = ('%,' * len(self.fnl))[:-1]
+        tokens = []
+        for fieldname in self.fnl:
+            tokens.append(self.data[n][fieldname])
+        
+        sql = 'INSERT INTO `' + self.tn + '+' (+cols)
+        #conn = pymysql.connect(host='mysql.clarksonmsda.org', port=3306, user='yuxchen', passwd='Phpmyadmin1', db='yuxchen_is437test', autocommit=True)
+        self.connect() 
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+        print(sql)
+        print(tokens)
--- unitTest.py
+++ unitTest.py
@@ -5,8 +5,11 @@ cl = customerList()
 # 'email':'a@a.com','password':'123','subscribed':'1'}
 # cl.add(d)
 cl.set('fname','Testguy')
-cl.set('lname', 'Test')
+cl.set('lname','Test')
+cl.set('lastname','Test')
 cl.set('email','a@a.com')
+cl.set('subscribed','True')
+cl.set('password',true)
 cl.add()
 cl.add()
 
@@ -14,4 +17,8 @@ print(cl.data)
 print(cl.data[0])
 # cl.data[0]['email'] = 'b@b.com'
 cl.update(0,'email','b@b.com')
-print(cl.data[0]['email'])
+cl.update(0,'e_mail','b@b.com')
+#print(cl.data[0]['email'])
+print(cl.data)
+
+cl.insert()
